a073ce7270a5e67338b7cbc651321584deb5e2a5
collinswebdev-dotcom <lovecollinsukwuoma@gmail.com>
2026-08-18 23:30:14 +0100

    Color.kt

---
+++ Color.kt
@@ -0,0 +1,37 @@
+package com.lunaflow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Pink Palette
+val PinkPrimary = Color(0xFFE91E63)
+val PinkLight = Color(0xFFF8BBD0)
+val PinkDark = Color(0xFFC2185B)
+val PinkBackground = Color(0xFFFFF5F7)
+
+// Secondary Colors
+val Lavender = Color(0xFFE1BEE7)
+val SoftPurple = Color(0xFF9C27B0)
+val WarmGold = Color(0xFFD4AF37)
+
+// Neutrals
+val CreamWhite = Color(0xFFFFFDF9)
+val SoftGray = Color(0xFFF5F5F5)
+val Charcoal = Color(0xFF2C2C2C)
+val MutedText = Color(0xFF757575)
+
+// Functional Colors
+val SuccessGreen = Color(0xFF4CAF50)
+val WarningAmber = Color(0xFFFFC107)
+val ErrorRed = Color(0xFFF44336)
+
+// Calendar Colors
+val PeriodDay = Color(0xFFE91E63)
+val FertileDay = Color(0xFF4CAF50)
+val OvulationDay = Color(0xFF2196F3)
+val PredictedPeriod = Color(0xFFFFB6C1)
+val NormalDay = Color(0xFFFFFFFF)
+
+// Dark Mode Colors
+val DarkPinkPrimary = Color(0xFFFF6090)
+val DarkBackground = Color(0xFF1A1A1A)
+val DarkSurface = Color(0xFF2D2D2D)
