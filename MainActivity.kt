9e6e8625b50b64a555f4c6389d7ff94c65a45414
collinswebdev-dotcom <lovecollinsukwuoma@gmail.com>
2026-08-18 23:41:11 +0100

    MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,29 @@
+package com.lunaflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.lunaflow.ui.navigation.NavGraph
+import com.lunaflow.ui.theme.LunaFlowTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LunaFlowTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
